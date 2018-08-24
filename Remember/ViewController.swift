298096bf348afbd32b44a1df6472df71456c1770
Le Duy Bach <bachld@vng.com.vn>
2018-08-24 13:37:51 +0700

    Initial Commit

---
+++ Remember/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Remember
+//
+//  Created by CPU12071 on 8/24/18.
+//  Copyright © 2018 Le Duy Bach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
